2becd0100a09510c35818fd882bbbcbf6c046f5f
f-binas <fivosdude@hotmail.com>
2016-02-09 19:37:01 +0200

    first v

---
+++ Hello/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Hello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageBtn
+            // 
+            this.messageBtn.Location = new System.Drawing.Point(592, 250);
+            this.messageBtn.Name = "messageBtn";
+            this.messageBtn.Size = new System.Drawing.Size(75, 23);
+            this.messageBtn.TabIndex = 0;
+            this.messageBtn.Text = "Message";
+            this.messageBtn.UseVisualStyleBackColor = true;
+            this.messageBtn.Click += new System.EventHandler(this.messageBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 572);
+            this.Controls.Add(this.messageBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button messageBtn;
+    }
+}
+
